f01e1e8db39c1f29416aaf08d07182d6c7ca0310
Koji HIJIKURO <koji0903@gmail.com>
2013-03-08 18:08:03 +0900

    20130308

--- apps/bin/FX.rb
+++ apps/bin/FX.rb
@@ -71,7 +71,7 @@ class FX
     printf "@I:Generate Historical Data to TXT\n"
     viewer.generate_TechnicalData
     printf "@I:Generate Histrrical Data to Excel\n"
-#    viewer.generate_Excel
+    viewer.generate_Excel
 #    printf "@I:Generate Analyzed Data to CSV\n"
 #    viewer.generate_Trade
     printf "@I:Generate Graph\n"
