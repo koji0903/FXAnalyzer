5b87cb63fc26c6940ab1fc89bbc4b25ba9219e51
Koji HIJIKURO <koji0903@gmail.com>
2014-02-27 10:43:21 +0900

    support ruby2.1.1

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-RUBY=/Users/koji/.rbenv/versions/1.9.3-p448/bin/ruby
+#RUBY=/Users/koji/.rbenv/versions/1.9.3-p448/bin/ruby
+RUBY=/Users/koji/.rbenv/versions/2.1.1/bin/ruby
 BASE=/Users/koji/workspace/FXAnalyzer/apps
 FX=${BASE}/bin/FX.rb
 
